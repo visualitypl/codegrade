08ffde9e7100f27462cf24259ab89a9ed813a14d
Karol Sluszniak <ksluszniak@gmail.com>
2014-12-05 16:34:40 +0100

    Refactor the commit message grader code
    * Now single state variable is passed to all checkers and it's in
      the checked where all conditions are validated, thus eliminating
      the confusing if/else branches in line loop

--- lib/codegrade/grader/commit_message.rb
+++ lib/codegrade/grader/commit_message.rb
@@ -20,199 +20,213 @@ module Codegrade
 
       def parse_commit_message
         lines = @message.split("\n")
-
-        paragraph, paragraph_start, paragraph_line = [], 1, 0
-        inside_punctation = false
+        state = {
+          :paragraph         => [],
+          :paragraph_start   => 1,
+          :inside_punctation => false }
 
         lines.each_with_index do |line, index|
-          paragraph << line
-          paragraph_line += 1
+          state[:paragraph] << line
 
-          line_number      = index + 1
-          blank_line       = blank?(line)
-          title_line       = line_number == 1
-          end_of_paragraph = (index == lines.length - 1 ||
-                             blank?(lines[index + 1])) && !title_line &&
-                             paragraph.any? && !blank?(paragraph.last)
+          state.merge!(
+            :line                => line,
+            :index               => index,
+            :line_number         => index + 1,
+            :blank_line          => blank?(line),
+            :title_line          => index == 0,
+            :start_of_punctation => line.start_with?('* '),
+            :was_in_punctation   => state[:inside_punctation])
 
-          if line.start_with?('* ')
-            check_punctation_no_separating_line(inside_punctation, line_number)
+          state[:end_of_paragraph] = !state[:title_line] &&
+            state[:paragraph].any? &&
+            (index == lines.length - 1 || blank?(lines[index + 1])) &&
+            !blank?(state[:paragraph].last)
 
-            inside_punctation = true
-          end
+          state[:inside_punctation] = state[:inside_punctation] ||
+            state[:start_of_punctation]
 
-          if title_line
-            check_title_leading_lowercase(line, line_number)
-            check_title_too_long(line, line_number)
-            check_title_trailing_dot(line, line_number)
-          end
+          check_title_leading_lowercase(state)
+          check_title_too_long(state)
+          check_title_trailing_dot(state)
+          check_title_multiple_lines(state)
 
-          if end_of_paragraph
-            if inside_punctation
-              check_punctation_leading_lowercase(paragraph, paragraph_start)
-              check_punctation_trailing_dot(paragraph, paragraph_start)
-            else
-              check_paragraph_leading_lowercase(paragraph, paragraph_start)
-              check_paragraph_no_trailing_dot(paragraph, paragraph_start)
-            end
-          end
+          check_line_trailing_whitespace(state)
+          check_line_too_long(state)
+          check_line_leading_whitespace(state)
+          check_redundant_empty_line(state)
 
-          if blank_line
-            check_redundant_empty_line(paragraph, line_number)
+          check_punctation_leading_whitespace(state)
+          check_punctation_leading_lowercase(state)
+          check_punctation_trailing_dot(state)
+          check_punctation_no_separating_line(state)
 
-            paragraph, paragraph_start, paragraph_line = [], line_number + 1, 0
-            inside_punctation = false
-          else
-            check_title_multiple_lines(line_number)
+          check_paragraph_leading_lowercase(state)
+          check_paragraph_no_trailing_dot(state)
 
-            check_line_trailing_whitespace(line, line_number)
-            check_line_too_long(line, line_number) unless title_line
-          end
-
-          if inside_punctation
-            if paragraph_line > 1
-              check_punctation_leading_whitespace(line, line_number)
-            end
-          else
-            if ! blank_line
-              check_line_leading_whitespace(line, line_number)
-            end
+          if state[:blank_line]
+            state.merge!(
+              :paragraph         => [],
+              :paragraph_start   => state[:line_number] + 1,
+              :inside_punctation => false)
           end
         end
       end
 
-      def check_title_leading_lowercase(line, line_number)
-        if line[0].downcase == line[0]
+      def check_title_leading_lowercase(state)
+        if state[:title_line] && state[:line][0].downcase == state[:line][0]
           add_offense(
             :category       => 'title_leading_lowercase',
-            :line_number    => line_number,
+            :line_number    => state[:line_number],
             :column_number  => 1)
         end
       end
 
-      def check_title_too_long(line, line_number)
-        if line.length > 50
+      def check_title_too_long(state)
+        if state[:title_line] && state[:line].length > 50
           add_offense(
             :category      => 'title_too_long',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => 51)
         end
       end
 
-      def check_title_trailing_dot(line, line_number)
-        if (m = line.match(/\.\s*$/))
+      def check_title_trailing_dot(state)
+        if state[:title_line] && (m = state[:line].match(/\.\s*$/))
           add_offense(
             :category      => 'title_trailing_dot',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => m.begin(0))
         end
       end
 
-      def check_title_multiple_lines(line_number)
-        if line_number == 2
+      def check_title_multiple_lines(state)
+        return if state[:blank_line]
+
+        if state[:line_number] == 2
           add_offense(
             :category    => 'title_multiple_lines',
-            :line_number => line_number)
+            :line_number => state[:line_number])
         end
       end
 
-      def check_redundant_empty_line(paragraph, line_number)
-        if paragraph.select { |line| !blank?(line) }.empty?
+      def check_redundant_empty_line(state)
+        return unless state[:blank_line]
+
+        if state[:paragraph].select { |line| !blank?(line) }.empty?
           add_offense(
             :category       => 'redundant_empty_line',
-            :line_number    => line_number)
+            :line_number    => state[:line_number])
         end
       end
 
-      def check_line_trailing_whitespace(line, line_number)
-        if (m = line.match(/\s+$/))
+      def check_line_trailing_whitespace(state)
+        return if state[:blank_line]
+
+        if (m = state[:line].match(/\s+$/))
           add_offense(
             :category      => 'line_trailing_whitespace',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => m.begin(0) + 1)
         end
       end
 
-      def check_line_leading_whitespace(line, line_number)
-        if (m = line.match(/^\s+/))
+      def check_line_leading_whitespace(state)
+        if !state[:inside_punctation] &&
+            !state[:blank_line] &&
+            (m = state[:line].match(/^\s+/))
           add_offense(
             :category      => 'line_leading_whitespace',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => m.end(0))
         end
       end
 
-      def check_line_too_long(line, line_number)
-        if line.length > 70
+      def check_line_too_long(state)
+        return if state[:blank_line] || state[:title_line]
+
+        if state[:line].length > 70
           add_offense(
             :category      => 'line_too_long',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => 71)
         end
       end
 
-      def check_paragraph_leading_lowercase(paragraph, paragraph_start)
-        line = strip(paragraph.first)
+      def check_paragraph_leading_lowercase(state)
+        return unless state[:end_of_paragraph] && !state[:inside_punctation]
+
+        line = strip(state[:paragraph].first)
 
         if line[0].downcase == line[0] &&
             !link?(line.split[0])
           add_offense(
             :category      => 'paragraph_leading_lowercase',
-            :line_number   => paragraph_start,
+            :line_number   => state[:paragraph_start],
             :column_number => 1)
         end
       end
 
-      def check_paragraph_no_trailing_dot(paragraph, paragraph_start)
-        line = strip(paragraph.last)
+      def check_paragraph_no_trailing_dot(state)
+        return unless state[:end_of_paragraph] && !state[:inside_punctation]
+
+        line = strip(state[:paragraph].last)
 
         if line[-1] != '.' &&
             !link?(line.split[-1])
           add_offense(
             :category      => 'paragraph_no_trailing_dot',
-            :line_number   => paragraph_start + paragraph.length - 1,
-            :column_number => paragraph.last.length)
+            :line_number   => state[:paragraph_start] +
+                              state[:paragraph].length - 1,
+            :column_number => state[:paragraph].last.length)
         end
       end
 
-      def check_punctation_no_separating_line(inside_punctation, line_number)
-        if inside_punctation
+      def check_punctation_no_separating_line(state)
+        if state[:start_of_punctation] && state[:was_in_punctation]
           add_offense(
             :category      => 'punctation_no_separating_line',
-            :line_number   => line_number)
+            :line_number   => state[:line_number])
         end
       end
 
-      def check_punctation_leading_whitespace(line, line_number)
-        if (m = line.match(/^\s*/)) && m.end(0) != 2
+      def check_punctation_leading_whitespace(state)
+        if state[:inside_punctation] &&
+            !state[:blank_line] &&
+            state[:paragraph].length > 1 &&
+            (m = state[:line].match(/^\s*/)) && m.end(0) != 2
           add_offense(
             :category      => 'punctation_leading_whitespace',
-            :line_number   => line_number,
+            :line_number   => state[:line_number],
             :column_number => m.end(0))
         end
       end
 
-      def check_punctation_leading_lowercase(paragraph, paragraph_start)
-        line = strip(paragraph.first)
+      def check_punctation_leading_lowercase(state)
+        return unless state[:end_of_paragraph] && state[:inside_punctation]
+
+        line = strip(state[:paragraph].first)
 
         if line[2].downcase == line[2] &&
             !link?(line.split[0])
           add_offense(
             :category      => 'punctation_leading_lowercase',
-            :line_number   => paragraph_start,
+            :line_number   => state[:paragraph_start],
             :column_number => 3)
         end
       end
 
-      def check_punctation_trailing_dot(paragraph, paragraph_start)
-        line = strip(paragraph.last)
+      def check_punctation_trailing_dot(state)
+        return unless state[:end_of_paragraph] && state[:inside_punctation]
+
+        line = strip(state[:paragraph].last)
 
         if line[-1] == '.' &&
             !link?(line.split[-1])
           add_offense(
             :category      => 'punctation_trailing_dot',
-            :line_number   => paragraph_start + paragraph.length - 1,
-            :column_number => paragraph.last.length)
+            :line_number   => state[:paragraph_start] +
+                              state[:paragraph].length - 1,
+            :column_number => state[:paragraph].last.length)
         end
       end
 
